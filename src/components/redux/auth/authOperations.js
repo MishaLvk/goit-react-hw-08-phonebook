3bffbb54e897221ad0158eaff13b71247675a41a
Misha Levko <lvk.misha@gmail.com>
2022-12-16 12:20:00 +0200

    fixed logOut

--- src/components/redux/auth/authOperations.js
+++ src/components/redux/auth/authOperations.js
@@ -43,7 +43,7 @@ export const logOut = createAsyncThunk(
   'auth/logout',
   async (credentials, thunkAPI) => {
     try {
-      const response = await axios.post('/users/logout');
+      await axios.post('/users/logout');
       clearAuthHeader();
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
